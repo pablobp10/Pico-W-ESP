9724cd1cc4e6ea70d288944a63fe9b0bceb4bc50
pablobp10 <68905378+pablobp10@users.noreply.github.com>
2026-03-04 14:17:41 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,26 @@ self.addEventListener('install', (e) => {
     self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-    // Este evento es obligatorio pra que el navegador te considere una App nativa
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        // 1. ESTRATEGIA NETWORK-FIRST (Red Primero)
+        fetch(event.request)
+            .then((respuestaRed) => {
+                // Si hay internet y la respuesta es válida, actualizamos el búnker silenciosamente
+                if (respuestaRed && respuestaRed.status === 200 && respuestaRed.type === 'basic') {
+                    const respuestaClonada = respuestaRed.clone();
+                    caches.open('pico-os-auto-cache').then((cache) => {
+                        cache.put(event.request, respuestaClonada);
+                    });
+                }
+                // Entregamos el archivo fresco al usuario
+                return respuestaRed;
+            })
+            .catch(() => {
+                // 2. MODO OFFLINE (Rescate)
+                // Si no hay internet o la red falla, tiramos de la memoria caché
+                return caches.match(event.request);
+            })
+    );
 });
 
